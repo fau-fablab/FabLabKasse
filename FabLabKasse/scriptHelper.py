beba75e02e3d6844d82988cf24198ee027cfc347
Patrick Kanzler <patrick.kanzler@fablab.fau.de>
2015-07-04 14:58:05 +0200

    IMPROVE crash dialog from scriptHelper.py
     * closes #34
     * former idea bold red was bad, now the information is grouped by the integrated means of QTMessageBox
     * the message is now clearly a critical error and not YetAnotherDialog(TM)
     * I slightly refactored scriptHelper.py in accordance to PEP8
     * If my refactoring introduced a defect, the finder will get a Mate

--- FabLabKasse/scriptHelper.py
+++ FabLabKasse/scriptHelper.py
@@ -4,6 +4,7 @@
 # FabLabKasse, a Point-of-Sale Software for FabLabs and other public and trust-based workshops.
 # Copyright (C) 2014  Julian Hammer <julian.hammer@fablab.fau.de>
 #                     Maximilian Gaukler <max@fablab.fau.de>
+#                     Patrick Kanzler <patrick.kanzler@fablab.fau.de>
 #                     Timo Voigt <timo@fablab.fau.de>
 # 
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU
@@ -30,19 +31,20 @@ import time
 from PyQt4 import QtGui
 import traceback
 
-def setupLogging(logfile):
-    my_logger=logging.getLogger()
-     # rotate every day at 00:00, delete after 14 days
-    handler = logging.handlers.TimedRotatingFileHandler(logfile, when='midnight', interval=1, backupCount=14)
-    #handler = logging.FileHandler('example.logfile')
-    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', '%Y-%m-%d %H:%M:%S'))
-    my_logger.setLevel(0) # change this to 0 to log everything, even DEBUG-1, change to DEBUG to limit the amount of useless messages
-    my_logger.addHandler(handler)
-    consoleHandler=logging.StreamHandler() # log to stderr
-    consoleHandler.setLevel(0) # log everything, even DEBUG-1
-    my_logger.addHandler(consoleHandler)
-    my_logger.info("started logging to "+logfile)
 
+def setupLogging(logfile):
+    """configures the logging and logrotation"""
+    my_logger = logging.getLogger()
+    # rotate every day at 00:00, delete after 14 days
+    handler = logging.handlers.TimedRotatingFileHandler(logfile, when='midnight', interval=1, backupCount=14)
+    # handler = logging.FileHandler('example.logfile')
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', '%Y-%m-%d %H:%M:%S'))
+    my_logger.setLevel(0)  # change this to 0 to log everything, even DEBUG-1, change to DEBUG to limit the amount of useless messages
+    my_logger.addHandler(handler)
+    consolehandler = logging.StreamHandler()  # log to stderr
+    consolehandler.setLevel(0)  # log everything, even DEBUG-1
+    my_logger.addHandler(consolehandler)
+    my_logger.info("started logging to "+logfile)
 
 
 def setupSigInt():
@@ -51,27 +53,33 @@ def setupSigInt():
         sys.exit(1)
     signal.signal(signal.SIGINT, sigint)
 
+
 def setupGraphicalExceptHook():
-    "open a Qt messagebox on fatal exceptions"
+    """open a Qt messagebox on fatal exceptions"""
     if "--debug" in sys.argv:
         # we don't want this when running in a debugger
         return
-    sys.excepthook_old=sys.excepthook
+    sys.excepthook_old = sys.excepthook
+
     def myNewExceptionHook(exctype, value, tb):
         import datetime
-        #logging.exception()
+        # logging.exception()
         try:
-            msgBox = QtGui.QMessageBox()
-            txt = u"Entschuldigung, das Programm wird wegen eines Fehlers beendet. \n"
-            txt += u" Wenn dir Rückgeld entgangen ist, melde dich bei kasse@fablab.fau.de und gebe " + \
-                   u"folgende Uhrzeit an:"
-            txt += u"{}.\n{}".format(str(datetime.datetime.today()),"".join(
+            msgbox = QtGui.QMessageBox()
+            txt = u"Entschuldigung, das Programm wird wegen eines Fehlers beendet."
+            infotxt = u"Wenn dir Rückgeld entgangen ist, melde dich bei kasse@fablab.fau.de und gebe " + \
+                u"neben einer Fehlerbeschreibung folgende Uhrzeit an: "
+            infotxt += u"\n{}.".format(str(datetime.datetime.today()))
+            detailtxt = u"{}\n{}".format(str(datetime.datetime.today()), "".join(
                 traceback.format_exception(exctype, value, tb, limit=10)))
             logging.fatal(txt)
             logging.fatal(u"Full exception details (stack limit 50):\n"+u"".join(
                 traceback.format_exception(exctype, value, tb, limit=50)))
-            msgBox.setText(txt)
-            msgBox.exec_()
+            msgbox.setText(txt)
+            msgbox.setInformativeText(infotxt)
+            msgbox.setDetailedText(detailtxt)
+            msgbox.setIcon(QtGui.QMessageBox.Critical)
+            msgbox.exec_()
         except Exception as e:
             try:
                 logging.error("graphical excepthook failed: " + repr(e))
@@ -79,7 +87,8 @@ def setupGraphicalExceptHook():
                 logging.error("graphical excepthook failed hard,  cannot print exception (IOCHARSET problems?)")
         sys.excepthook_old(exctype, value, tb)
         sys.exit(1)
-    sys.excepthook=myNewExceptionHook
+    sys.excepthook = myNewExceptionHook
+
 
 def getConfig(path="./"):
     cfg = ConfigParser()
@@ -89,24 +98,28 @@ def getConfig(path="./"):
         raise Exception("Cannot open configuration file. If you want to try the program and do not have a config, start ./run.py --example or just copy config.ini.example to config.ini")
     return cfg
 
+
 def getDB():
-    cfg=getConfig()
+    cfg = getConfig()
     return sqlite3.connect(cfg.get('general', 'db_file'))
+
 
 class FileLock(object):
     """
     exclusive file-lock, for locking a resource against other processes
     """
     def __init__(self, name):
-        self.f=open(name+".lock", "w")
+        self.f = open(name+".lock", "w")
         try:
-            fcntl.flock(self.f.fileno(),  fcntl.LOCK_EX|fcntl.LOCK_NB)
+            fcntl.flock(self.f.fileno(),  fcntl.LOCK_EX | fcntl.LOCK_NB)
         except IOError:
             raise Exception("lock " + name + " already taken, is another process already running?")
+
+
 class Timer:
     def __init__(self, name):
         self.name = name
-        
+
     def __enter__(self):
         self.start = time.clock()
         return self
