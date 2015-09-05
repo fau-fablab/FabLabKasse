53f50dae71e0f0c1cd90b48de215ea644b6795d2
cod3monk <codemonk@u-sys.org>
2015-09-05 18:07:55 +0200

    FIXED #65 (doctest in moneyfmt is failing)

--- FabLabKasse/kassenbuch.py
+++ FabLabKasse/kassenbuch.py
@@ -57,11 +57,11 @@ import doctest
 import libs.escpos.printer as escpos_printer
 
 
-def moneyfmt(value, places=2, curr='', sep=',', dp=',',
+def moneyfmt(value, places=2, curr='', sep='.', dp=',',
              pos='', neg='-', trailneg=''):
     """Convert Decimal to a money formatted string.
     ::
-
+    
         :param places:  required number of places after the decimal point
         :param curr:    optional currency symbol before the sign (may be blank)
         :param sep:     optional grouping separator (comma, period, space, or blank)
@@ -73,16 +73,17 @@ def moneyfmt(value, places=2, curr='', sep=',', dp=',',
 
         >>> d = Decimal('-1234567.8901')
         >>> moneyfmt(d, curr='$')
-        '-$1,234,567.89'
-        >>> moneyfmt(d, places=0, sep='.', dp='', neg='', trailneg='-')
-        '1.234.568-'
+        '-$1.234.567,89'
+        >>> moneyfmt(d, places=0, sep=',', dp='', neg='', trailneg='-')
+        '1,234,568-'
         >>> moneyfmt(d, curr='$', neg='(', trailneg=')')
-        '($1,234,567.89)'
+        '($1.234.567,89)'
         >>> moneyfmt(Decimal(123456789), sep=' ')
-        '123 456 789.00'
+        '123 456 789,00'
         >>> moneyfmt(Decimal('-0.02'), neg='<', trailneg='>')
-        '<0.02>'
-
+        '<0,02>'
+        
+        Based on https://docs.python.org/2/library/decimal.html
     """
     q = Decimal(10) ** -places  # 2 places --> '0.01'
     sign, digits, exp = value.quantize(q).as_tuple()
