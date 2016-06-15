3c8a7cf5abec7174321913d5130ca5b23ac9df52
sedrubal <dev@sedrubal.de>
2016-06-15 20:25:33 +0200

    Grml python2

--- FabLabKasse/kassenbuch.py
+++ FabLabKasse/kassenbuch.py
@@ -28,8 +28,6 @@ Kassenbuch Backend mit doppelter Buchführung.
 """
 
 from __future__ import print_function
-from __future__ import unicode_literals
-from __future__ import absolute_import
 
 import sqlite3
 import argparse
