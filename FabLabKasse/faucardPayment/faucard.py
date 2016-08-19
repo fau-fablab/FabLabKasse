b31180b1b3225bedea5509635528b871eea2cfba
Bernhard Drescher <drescherbernhard@yahoo.de>
2016-08-19 17:34:56 +0200

    ADD FAUCardPayment functionality with MagPOS as stub

---
+++ FabLabKasse/faucardPayment/faucard.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+#
+
+import codecs
+import scriptHelper
+from ConfigParser import ConfigParser
+
+import sqlite3
+from datetime import datetime
+from decimal import Decimal
+from PyQt4 import QtCore, Qt, QtGui
+
+from MagPosLog import MagPosLog
+from FAUcardPaymentDialogCode import FAUcardPaymentDialog
+from FAUcardPaymentThread import FAUcardThread
+from faucardStates import Status, Info
+
+
+cfg = scriptHelper.getConfig()
+cfg.readfp(codecs.open('config.ini', 'r', 'utf8'))
+
+
+class PayupFAUCard(QtCore.QObject):
+    def __init__(self, parent, amount):
+        """
+        Initializes the PayupFAUCard Process
+        :param parent: Parent QObject
+        :type parent: QtCore.QObject
+        :param amount: amount to pay
+        :type amount: Decimal float
+        """
+        QtCore.QObject.__init__(self)
+        assert isinstance(amount, (Decimal, float)), "PayupFAUCard: Amount to pay not Decimal or float"
+        assert amount > 0, "PayupFAUCard: amount is negativ"
+
+        self.amount = float(amount)
+        self.thread = QtCore.QThread()
+        self.dialog = FAUcardPaymentDialog(parent=parent, amount=self.amount)
+        self.dialog.request_termination.connect(self.threadTerminationRequested, type= QtCore.Qt.DirectConnection)
+        self.worker = FAUcardThread(dialog=self.dialog, amount=self.amount, thread=self.thread)
+        self.want_receipt = False
+
+    def executePayment(self):
+        """
+        Starts the Payment-process and Dialog
+        :return: True on success, False otherwise
+        :rtype: bool
+        """
+        # Start the process
+        self.thread.start()
+        self.dialog.show()
+
+        # Execute the GUI
+        success = self.dialog.exec_()
+
+        if success == Qt.QDialog.Accepted:
+            receipt = QtGui.QMessageBox.question(self.parent(), u'FaucardPayment', u'Brauchst du eine Rechnung?', QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
+            if receipt == QtGui.QMessageBox.Yes:
+                self.want_receipt = True
+            return True
+        else:
+            # Wait for thread to finish cleanup
+            self.thread.wait(1000)
+            return False
+
+    def getPaidAmount(self):
+        """
+        Returns the amount the user has payed with his FAUCard
+        :return: the amount payed
+        :rtype: Decimal
+        """
+        return self.worker.get_amount_payed()
+
+    def getWantReceipt(self):
+        """
+        Returns if the user wants a receipt
+        :return: bool if user wants receipt
+        :rtype: bool
+        """
+        return self.want_receipt
+
+    def finishLogEntry(self):
+        """
+        Completes the log entry in the MagPosLog and closes all open threads and dialogs
+        """
+        if self.thread.isRunning():
+            QtCore.QMetaObject.invokeMethod(self.worker, "set_ack", QtCore.Qt.QueuedConnection, Qt.Q_ARG(bool, False))
+        self.close()
+
+    @QtCore.pyqtSlot()
+    def threadTerminationRequested(self):
+        """
+        Terminates the self.thread if requested.
+        """
+        self.thread.wait(100)
+        self.thread.quit()
+        if not self.thread.wait(1000):
+            self.thread.terminate()
+
+    def close(self):
+        """
+        Quits self.thread, closes self.dialog
+        """
+        self.dialog.close()
+        if self.thread.isRunning():
+            QtCore.QMetaObject.invokeMethod(self.worker, "set_should_finish_log", QtCore.Qt.QueuedConnection, Qt.Q_ARG(bool, False))
+            QtCore.QMetaObject.invokeMethod(self.worker, "set_ack", QtCore.Qt.QueuedConnection, Qt.Q_ARG(bool, False))
+            self.thread.wait(100)
+            if not self.thread.isRunning():
+                return
+            self.thread.quit()
+            if not self.thread.wait(1000):
+                self.thread.terminate()
+
+
+def check_last_transaction():
+    """
+    Dummy function to easier access check_last_transaction from code outside of faucard.py
+    :return: True on success reading last transaction details, False otherwise
+    :rtype: bool
+    """
+    con = sqlite3.connect(cfg.get('magna_carta', 'log_file'))
+    cur = con.cursor()
+    con.text_factory = unicode
+    return FAUcardThread.check_last_transaction(cur=cur, con=con)
+
+
+def finish_log(info = Info.OK):
+    """
+    Part
+    Finishes last MagPosLog Entry by setting its state to Status.booking_done after the internal booking was done
+    """
+    con = sqlite3.connect("magposlog.sqlite3")
+    cur = con.cursor()
+    con.text_factory = unicode
+
+    cur.execute("SELECT id, status, info FROM MagPosLog ORDER BY id DESC LIMIT 1")
+    row = cur.fetchone()
+
+    # Check if last entry was about an not yet booked but payed payment
+    if row[1] == Status.decreasing_done.value and row[2] == Info.OK.value:
+        id = row[0]
+        cur.execute("UPDATE MagPosLog SET datum=(?), status=(?), info=(?) WHERE id=(?)",
+                    (datetime.now(), Status.booking_done.value, info.value, id))
+        con.commit()
---
+++ FabLabKasse/faucardPayment/magpos/codes.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+
+OK = 0x00;
