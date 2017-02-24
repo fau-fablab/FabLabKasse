158ef8bb18edbd237afe90dcaf6cbf1ffd7d3c40
Bernhard Drescher <drescherbernhard@yahoo.de>
2017-02-24 19:46:29 +0100

    Added verify-sum function, Removed scripthelper usage by embedding MagnaBox Serial as constant

--- FabLabKasse/faucardPayment/generateLog.py
+++ FabLabKasse/faucardPayment/generateLog.py
@@ -1,17 +1,42 @@
+#!/usr/bin/env python
 import argparse
 from datetime import datetime, timedelta
 import sqlite3
 import codecs
 from decimal import Decimal
-from FabLabKasse import scriptHelper
+
+#from FabLabKasse import scriptHelper
 
 
 def valid_date(s):
+    ''' Returns a valid datetime from a string. Used for argparse datetime strings'''
     try:
         return datetime.strptime(s,"%Y-%m-%d_%H:%M:%S")
     except ValueError:
         msg = "Not a valid date: '{0}'.".format(s)
         raise argparse.ArgumentTypeError(msg)
+
+def verify_sum(curKb, start, end, magposSum):
+    ''' Checks wether the sum from the MagPosLog equals the Sum in Kassenbuch.
+        Possible Failures are a missing transaction in the Kassenbuch if Kasse crashed
+        after payment'''
+    kbSumme = Decimal(0);
+    try:
+        curKb.execute("SELECT betrag FROM buchung WHERE konto = 'MagnaCarta' AND datum BETWEEN ? AND ? ",(start, end));
+        for row in curKb.fetchall():
+            kbSumme += Decimal(row[0]).quantize(Decimal('.01'));
+
+        if kbSumme != magposSum:
+            print "Sum Difference: MagPos: {0} \tKassenbuch: {1}".format(magposSum, kbSumme);
+        else:
+            print "No Sum Difference between MagPosLog and Kassenbuch"
+        return kbSumme == magposSum;
+
+    except sqlite3.OperationalError as e:
+        print "Failed to verify sum: {0}".format(e);
+        return False;
+    return False;
+
 
 
 if __name__ == '__main__':
@@ -23,6 +48,7 @@ if __name__ == '__main__':
     parser.add_argument('-e', "--enddate", help = "The End Date - format YYYY-MM-DD_HH:MM:SS", required=False, type=valid_date)
     parser.add_argument('-o', "--outputpath", help="Output path of csv and summary, e.g. /usr/var/test -> test.csv, testsummary.txt", required=True)
     parser.add_argument('-k', "--kassenbuch", help="Kassenbuch to build log from", required=True)
+    parser.add_argument('-d', "--detail", help="Enables the output of an detailed CSV containing the single positions per Rechnung", const=True, default=False, nargs = '?' )
 
     try:
         args = parser.parse_args()
@@ -38,7 +64,10 @@ if __name__ == '__main__':
     startdate = args.startdate
     enddate = args.enddate
     summe = Decimal(0)
-    cfg = scriptHelper.getConfig()
+
+    #cfg = scriptHelper.getConfig()
+
+    print "Building Summary from {0} to {1}".format(startdate,enddate);
 
     try:
         con = sqlite3.connect(args.file)
@@ -79,7 +108,7 @@ if __name__ == '__main__':
                 lastdate = timestamp
 
             # Determine corresponding rechnung
-            curKb.execute("SELECT rechnung FROM buchung WHERE (betrag = ? OR betrag = ?) AND datum BETWEEN ? AND ? ",(unicode(row[3]), "{:.3f}".format(row[3]), timestamp, timestamp + timedelta(seconds=20)))
+            curKb.execute("SELECT rechnung FROM buchung WHERE konto = 'MagnaCarta' AND (betrag = ? OR betrag = ?) AND datum BETWEEN ? AND ? ",(unicode(row[3]), "{:.3f}".format(row[3]), timestamp, timestamp + timedelta(seconds=20)))
             safetyCounter = 0
             rechnungsnr = -1
 
@@ -88,7 +117,8 @@ if __name__ == '__main__':
                 rechnungsnr = rowKb[0]
 
             # There should only be one result. Otherwise throw exception.
-            assert safetyCounter == 1
+            assert safetyCounter == 1, \
+                "Query for amount {0} at timestamp {1} failed.".format(row[3], timestamp);
 
             # Write CSV-Line
             line = u"{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}{6}\n".format(seperator, timestamp.strftime("%d-%m-%Y %H:%M:%S"), row[1], row[2], row[3], row[4], rechnungsnr)
@@ -101,19 +131,20 @@ if __name__ == '__main__':
         outputfile.close()
 
 
-        # Open detailed positons csv file
-        outputfile = codecs.open(args.outputpath+"_positions.csv", 'w', encoding="utf-8")
-        outputfile.write(u"Rechnung, Menge, Einzelpreis, Gesamtpreis\n")
-        outputfile.write(u",,,\n")
+        if args.detail is True:
+            # Open detailed positons csv file
+            outputfile = codecs.open(args.outputpath+"_positions.csv", 'w', encoding="utf-8")
+            outputfile.write(u"Rechnung, Menge, Einzelpreis, Gesamtpreis\n")
+            outputfile.write(u",,,\n")
 
-        for nr in rechnungsliste:
-            curKb.execute("SELECT anzahl, einzelpreis FROM position WHERE rechnung = ?", [nr])
+            for nr in rechnungsliste:
+                curKb.execute("SELECT anzahl, einzelpreis FROM position WHERE rechnung = ?", [nr])
 
-            for row in curKb.fetchall():
-                line = u"{1}{0}{2}{0}{3}{0}{4:.2f}\n".format(seperator, nr, row[0], row[1], float(row[0])*float(row[1]))
-                outputfile.write(line.encode('utf-8'))
+                for row in curKb.fetchall():
+                    line = u"{1}{0}{2}{0}{3}{0}{4:.2f}\n".format(seperator, nr, row[0], row[1], float(row[0])*float(row[1]))
+                    outputfile.write(line.encode('utf-8'))
 
-        outputfile.close()
+            outputfile.close()
 
 
         # Open Summary file
@@ -122,8 +153,14 @@ if __name__ == '__main__':
         # Write Summary file
         outputfile.write(u"Abrechnung bargeldloser Umsaetze - Akzeptanzstelle FAU FabLab\n")
         outputfile.write(u"Abrechnungszeitraum: {0} bis {1}\n".format(firstdate.strftime("%d-%m-%Y %H:%M:%S"), lastdate.strftime("%d-%m-%Y %H:%M:%S")))
-        outputfile.write(u"Seriennummer der MagnaBox: {0}\n".format(cfg.get('magna_carta', 'serial')))
+        outputfile.write(u"Seriennummer der MagnaBox: MB211475\n")#.format(cfg.get('magna_carta', 'serial')))
         outputfile.write(u"Der anfallende Betrag betraegt: {0}\n".format(summe))
+        if verify_sum(curKb, startdate, enddate, summe):
+            outputfile.write(u"Der Betrag im MagposLog entspricht dem im Kassenbuch: JA\n");
+        else:
+            outputfile.write(u"Der Betrag im MagposLog entspricht dem im Kassenbuch: NEIN\n");
+            print "VERIFY SUM FAILED";
+
     except IOError as e:
         print "ERROR: Saving CSV and / or Summary failed"
         print "IOERROR: {0}".format(e)
