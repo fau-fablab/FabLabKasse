5ba678f8e653b0b91da13989402788301c85e59b
Max Gaukler <development@maxgaukler.de>
2022-06-14 20:42:52 +0200

    FIX dummy shopping-backend

--- FabLabKasse/shopping/backend/dummy.py
+++ FabLabKasse/shopping/backend/dummy.py
@@ -98,6 +98,10 @@ class ShoppingBackend(AbstractOfflineShoppingBackend):
         AbstractOfflineShoppingBackend.__init__(
             self, cfg, categories, products, generate_root_category=True
         )
+        
+    def add_client(self, name, email, address, pin, comment, debt_limit):
+        # not implemented
+        pass
 
     def list_clients(self):
         clients = {
