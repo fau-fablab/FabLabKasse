f6a6842a384e5a6a4d43ed406e1873a6aca92277
Max Gaukler <development@maxgaukler.de>
2022-12-29 17:01:59 +0100

    FIX run_in_vagrant error "machine is locked"
    sleep times added to avoid "Vagrant can't use the requested machine because it is locked" errors

--- run_in_vagrant.sh
+++ run_in_vagrant.sh
@@ -5,12 +5,16 @@ set -ex
 # (note: 'vagrant provision' is not run automatically)
 vagrant up
 vagrant rsync
+# Note: sleep times are to avoid "Vagrant can't use the requested machine because it is locked" errors
+sleep 1
 
 # launch FAUCard emulator in background if available
 vagrant ssh --no-tty -c "echo Stopping FAUCard emulator; killall run_emulator.sh; echo Starting FAUCard emulator; /home/vagrant/FabLabKasse/FabLabKasse/faucardPayment/magpos/emulator/run_emulator.sh || echo FAUCard emulator not available or failed/exited" &
+sleep 1
 
 # launch printserver emulator in background
 vagrant ssh --no-tty -c "echo Stopping Dummy Printserver; killall dummy-printserver.py; echo Starting Dummy Printserver; /home/vagrant/FabLabKasse/FabLabKasse/tools/dummy-printserver.py || echo Dummy printserver failed/exited" &
+sleep 1
 
 # restart session
 vagrant ssh --no-tty -c "echo Starting GUI; echo; sudo service nodm restart"
