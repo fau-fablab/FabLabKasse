2832082995136c8107b72e9dd85ebf92cf46c55a
Max Gaukler <development@maxgaukler.de>
2023-06-09 12:42:49 +0200

    Try to fix run_in_vagrant: Printserver not working after restart

--- run_in_vagrant.sh
+++ run_in_vagrant.sh
@@ -9,11 +9,11 @@ vagrant rsync
 sleep 1
 
 # launch FAUCard emulator in background if available
-vagrant ssh --no-tty -c "echo Stopping FAUCard emulator; killall run_emulator.sh; echo Starting FAUCard emulator; /home/vagrant/FabLabKasse/FabLabKasse/faucardPayment/magpos/emulator/run_emulator.sh || echo FAUCard emulator not available or failed/exited" &
+vagrant ssh --no-tty -c "echo Stopping FAUCard emulator; killall -9 run_emulator.sh; echo Starting FAUCard emulator; /home/vagrant/FabLabKasse/FabLabKasse/faucardPayment/magpos/emulator/run_emulator.sh || echo FAUCard emulator not available or failed/exited" &
 sleep 1
 
 # launch printserver emulator in background
-vagrant ssh --no-tty -c "echo Stopping Dummy Printserver; killall dummy-printserver.py; echo Starting Dummy Printserver; /home/vagrant/FabLabKasse/FabLabKasse/tools/dummy-printserver.py || echo Dummy printserver failed/exited" &
+vagrant ssh --no-tty -c "echo Stopping Dummy Printserver; killall -9 dummy-printserver.py; echo Starting Dummy Printserver; /home/vagrant/FabLabKasse/FabLabKasse/tools/dummy-printserver.py || echo Dummy printserver failed/exited" &
 sleep 1
 
 # restart session
